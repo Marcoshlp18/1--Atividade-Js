d83d376ebc627fcdc142926bfe10a3bca82c0fe2
Marcoshlp18 <marcoshenriqueball12@gmail.com>
2022-06-09 21:32:50 -0300

    Js

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome= "Marcos";
+console.log(nome);
+let numero= 12;
+console.log(numero);
+let ano= 2022;
+console.log(ano-numero);
+console.log(ano+numero);
+let mensagem="Estou mexendo no console.log";
+console.log(mensagem);
+const quartaFeira=true;
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
